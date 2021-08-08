f3c1e1831e2a81b31bddb56234345c7fa720d3e1
jedi <amajidsinar@gmail.com>
2021-08-09 02:15:28 +0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log('App.js is running!')
+
+var template = <div><h1>Indecision App</h1> <p>This is some info</p></div>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot)
