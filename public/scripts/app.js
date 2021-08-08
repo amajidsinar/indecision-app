55a50bccf5e00380fb3d37d10aba6905a418d444
jedi <amajidsinar@gmail.com>
2021-08-09 04:06:52 +0700

    experiment with conditional rendering in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,85 @@
-'use strict';
+"use strict";
 
 console.log('App.js is running!');
 
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    "div",
     null,
-    'Indecision App'
-  ),
-  ' ',
-  React.createElement(
-    'p',
-    null,
-    'This is some info'
-  )
+    React.createElement(
+        "h1",
+        null,
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info"
+    ),
+    React.createElement(
+        "h1",
+        null,
+        "WTF"
+    )
 );
+
+var user = {
+    name: "wtf",
+    age: 27,
+    location: "mempool"
+};
+
+var template2 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "name: ",
+        user.name
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        user.location
+    )
+);
+
+var user = {
+    title: 'How To Kill A Mockingbird',
+    subtitle: 'Subtitle',
+    options: []
+};
+
+var template3 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Title: ",
+        user.title
+    ),
+    user.subtitle && React.createElement(
+        "p",
+        null,
+        "Subtitle: ",
+        user.subtitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        user.options.length > 0 ? "Here are your options" : "No options",
+        " "
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template3, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 console.log('App.js is running!')
 
-var template = <div><h1>Indecision App</h1> <p>This is some info</p></div>;
+var template = (
+    <div>
+        <h1>Indecision App</h1> 
+        <p>This is some info</p>
+        <h1>WTF</h1>
+    </div>
+);
+
+var user = {
+    name: "wtf",
+    age: 27,
+    location: "mempool"
+}
+
+var template2 = (
+    <div>
+        <h1>name: {user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
+var user = {
+    title: 'How To Kill A Mockingbird',
+    subtitle: 'Subtitle',
+    options: []
+};
+
+var template3 = (
+    <div>
+        <h1>Title: {user.title}</h1>
+        {user.subtitle && <p>Subtitle: {user.subtitle}</p>}
+        <p>{user.options.length > 0 ? "Here are your options" : "No options"} </p>
+    </div>
+);
+
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template3, appRoot)
